d5e349ec054f06ce612d4a0b8a0a2acdfa2511e8
kakau linda <165969632+kau4ne@users.noreply.github.com>
2024-06-05 21:42:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
+const caixaPrincipal = document.querySelector(“.caixa-
+principal”);
 
+const caixaPerguntas = document.querySelector(“.caixa-
+perguntas”);
 
-for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j=0; j<botoes.length; j++){
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
-}
+const caixaAlternativas = document.querySelector(“.caixa-
+alternativas”);
 
+const caixaResultado = document.querySelector(“.caixa-
+resultado”);
 
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-19T23:59:59");
- 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const textoResultado = document.querySelector(“.texto-
+resultado”);
 
-
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}
-
-
+const perguntas = [
+{
+enunciado: “Pergunta 1”,
+alternativas: [“Alternativa 1”, “Alternativa 2”],
+},
+{
+enunciado: “Pergunta 2”,
+alternativas: [“Alternativa 1”, “Alternativa 2”],
+},
+];
 
 
